f6240213d86103dbab482be97a151967a8a2aebf
Grupo <vinicius.janini@aluno.faculdadeimpacta.com.br>
2025-03-19 20:46:29 -0300

    fix: ajusta rotas no geral

--- spec/payloads.py
+++ spec/payloads.py
@@ -1,22 +1,23 @@
 from pydantic import BaseModel
-from typing import Optional
 
 class CreateAlunoPayload(BaseModel):
     id: int
     nome: str
+    idade: int
     data_nascimento: str
-    nota_primeiro_semestre: Optional[float] = None
-    nota_segundo_semestre: Optional[float] = None
-    media_final: Optional[float] = None
+    nota_primeiro_semestre: float
+    nota_segundo_semestre: float
+    media_final: float
     turma_id: int
 
 class UpdateAlunoPayload(BaseModel):
-    nome: Optional[str] = None
-    data_nascimento: Optional[str] = None
-    nota_primeiro_semestre: Optional[float] = None
-    nota_segundo_semestre: Optional[float] = None
-    media_final: Optional[float] = None
-    turma_id: Optional[int] = None
+    nome: str
+    idade: int
+    data_nascimento: str
+    nota_primeiro_semestre: float
+    nota_segundo_semestre: float
+    media_final: float
+    turma_id: int
 
 class CreateProfessorPayload(BaseModel):
     id: int
@@ -27,19 +28,19 @@ class CreateProfessorPayload(BaseModel):
     salario: float
 
 class UpdateProfessorPayload(BaseModel):
-    nome: Optional[str] = None
-    idade: Optional[int] = None
-    data_nascimento: Optional[str] = None
-    disciplina: Optional[str] = None
-    salario: Optional[float] = None
+    nome: str
+    idade: int
+    data_nascimento: str
+    disciplina: str
+    salario: float
 
 class CreateTurmaPayload(BaseModel):
     id: int
     nome: str
     turno: str
-    id_professor: int
+    professor_id: int
 
 class UpdateTurmaPayload(BaseModel):
-    nome: Optional[str] = None
-    turno: Optional[str] = None
-    id_professor: Optional[int] = None
+    nome: str
+    turno: str
+    professor_id: int
